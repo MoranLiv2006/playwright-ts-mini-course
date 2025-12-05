c4c8f556745fd8c0d8e33964cf2c2f2868d16eb8
Moran Liv <moran.liv2006@gmail.com>
2025-12-05 18:14:19 +0200

    adding basic project structure

---
+++ tests/sanity.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').dblclick();
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').dblclick();
+    await page.locator('[data-test="shopping-cart-link"]').click();
+    await page.locator('[data-test="checkout"]').click();
+    await page.locator('[data-test="firstName"]').click();
+    await page.locator('[data-test="firstName"]').fill('Moran');
+    await page.locator('.checkout_info').click();
+    await page.locator('[data-test="lastName"]').click();
+    await page.locator('[data-test="lastName"]').fill('Liv');
+    await page.locator('[data-test="lastName"]').press('Tab');
+    await page.locator('[data-test="postalCode"]').fill('123456');
+    await page.locator('[data-test="continue"]').click();
+    await page.locator('[data-test="finish"]').click();
+    await page.locator('[data-test="back-to-products"]').click();
+    await page.getByRole('button', { name: 'Open Menu' }).click();
+    await page.locator('[data-test="reset-sidebar-link"]').dblclick();
+    await page.locator('[data-test="logout-sidebar-link"]').click();
+});
