0d48df0927b8ef0f675b99ceea960f49f83162b2
Moran Liv <moran.liv2006@gmail.com>
2025-12-06 12:49:05 +0200

    Created enum class for the various error messages

---
+++ helpers/ErrorMessages.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessages {
+    LOGIN_WITH_LOCKED_USER = "Epic sadface: Sorry, this user has been locked out.\n",
+    LOGIN_WITH_INCORRECT_VALUE = "Epic sadface: Username and password do not match any user in this service\n",
+    LOGIN_WITH_MISSING_USERNAME = "Epic sadface: Username is required\n",
+    LOGIN_WITH_MISSING_PASSWORD = "Epic sadface: Password is required\n",
+}
