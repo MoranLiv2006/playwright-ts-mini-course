afc80cb54095a90b8b0f85e025299babe9456eec
Moran Liv <moran.liv2006@gmail.com>
2025-12-06 12:47:28 +0200

    Added an inventory URL const parameter

--- helpers/ApplicationURL.ts
+++ helpers/ApplicationURL.ts
@@ -1,3 +1,4 @@
 export default class ApplicationURL {
     public static readonly BASE_ULR = "https://www.saucedemo.com/";
+    public static readonly INVENTORY_PRODUCTS_PAGE_URL = `${this.BASE_ULR}inventory.html`;
 }
